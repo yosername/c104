d3db7d2d6eec8a8e900dbde3b85f10ba3763eb82
yosername <95049591+yosername@users.noreply.github.com>
2022-03-26 09:20:56 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format : 'png',
+    png_quality: 90
+});
+
+camera = document.getElementById("camera");
+
+
+Webcam.attach( '#camera ');
+
+
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+
+    });
+}
+
+
+console.log('ml5 versoion:', ml5.version);
